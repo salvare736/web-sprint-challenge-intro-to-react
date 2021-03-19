8b1774f706d4603f3f056bc99e6243804e915364
Sergio Alvarez <salvare736@gmail.com>
2021-03-19 13:09:06 -0400

    setup preliminary theme in 'index.js' and imported/exported all required data; also hit the API for the Star Wars dataset

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const Character = (props) => {
+    const { characterData } = props
+
+    return (
+        <div>
+            <p>Character component is rendering!</p>
+        </div>
+    );
+};
+
+export default Character;
---
+++ src/theme/index.js
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: '#f05454',
+    secondaryColor: '#30475e',
+    tertiaryColor: '#222831',
+    quaternaryColor: '#dddddd',
+    black: 'black',
+    white: 'white',
+}
